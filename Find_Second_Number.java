5d24205a0be91541698c1ce5ac811ada18c30279
Voorapavani <119840598+Voorapavani@users.noreply.github.com>
2024-01-05 21:43:24 +0530

    import java.util.Scanner; public class Area {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int c;         c=(2*x)-y;         System.out.println(c);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Area
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int c;
+        c=(2*x)-y;
+        System.out.println(c);
+    }
+}
