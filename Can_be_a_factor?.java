d6093667ddf48cf8e92b207c113c661c1d3a6e62
Voorapavani <119840598+Voorapavani@users.noreply.github.com>
2024-01-25 09:53:58 +0530

    import java.util.*; public class Factor {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         while(t-->0)         {             int a=sc.nextInt();             int b=sc.nextInt();             int z=Math.abs(a-b);             if(z>=a||a==b)             {                 System.out.println("YES");             }             else             {                 System.out.println("NO");             }         }     } }

---
+++ Can_be_a_factor?.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Factor
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int z=Math.abs(a-b);
+            if(z>=a||a==b)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
