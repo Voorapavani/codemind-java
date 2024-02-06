f96be95e7982ae45725d70cf8f5ad969aa8652f1
Voorapavani <119840598+Voorapavani@users.noreply.github.com>
2024-02-06 11:12:34 +0530

    import java.util.*; public class DragonFruit {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int p=sc.nextInt();         int t=sc.nextInt();         int r=sc.nextInt();         int s=(p*t*r)/100;         System.out.print(s);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class DragonFruit
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int t=sc.nextInt();
+        int r=sc.nextInt();
+        int s=(p*t*r)/100;
+        System.out.print(s);
+    }
+}
+
