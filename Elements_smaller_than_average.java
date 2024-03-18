ca690eee474d62f5b4a164dadeeb169f256363ab
Voorapavani <119840598+Voorapavani@users.noreply.github.com>
2024-03-18 11:32:21 +0530

    import java.util.Scanner; public class Elements{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=s.nextInt();         }         int sum=0;         for(int i=0;i<n;i++)         {             sum+=arr[i];         }         int c=0;         double avg=Math.floor(sum/n);         for(int i=0;i<n;i++){             if(arr[i]<=avg){                 c++;             }         }         System.out.println(c);     } }

---
+++ Elements_smaller_than_average.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Elements{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+        }
+        int c=0;
+        double avg=Math.floor(sum/n);
+        for(int i=0;i<n;i++){
+            if(arr[i]<=avg){
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
